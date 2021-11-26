063e13e2c4e9bd9140d33af21cb99cee67393fe1
Sarwang Jain <sarwangjain05@gmail.com>
2021-11-26 16:39:43 +0530

    Completed products Range

--- pages/products-range/index.js
+++ pages/products-range/index.js
@@ -3,6 +3,53 @@ import { CaptionHeading, RevealCard } from "@components";
 import styles from "./index.module.css";
 import { Pages } from "@enums";
 import gsap from "gsap";
+import globalStyles from "@styles/global.module.css";
+import Link from "next/link";
+import { mx } from "@utils";
+
+const sectors = [
+    {
+        name: "Mining",
+        image: "/images/sectors/mining.jpg",
+        description:
+            "Lorem ipsum dolor si amet, consectetur adipisicing elit. Rerum, quam deserunt accusantium architecto, molestias voluptates sequi repellendus alias ipsam nulla fuga deleniti maxime vero at labore modi natus velit maiores",
+        btnLink: "",
+    },
+    {
+        name: "Rigging",
+        image: "/images/sectors/rigging.jpg",
+        description:
+            "Lorem ipsum dolor si amet, consectetur adipisicing elit. Rerum, quam deserunt accusantium architecto, molestias voluptates sequi repellendus alias ipsam nulla fuga deleniti maxime vero at labore modi natus velit maiores",
+        btnLink: "",
+    },
+    {
+        name: "Piling",
+        image: "/images/sectors/piling.jpg",
+        description:
+            "Lorem ipsum dolor si amet, consectetur adipisicing elit. Rerum, quam deserunt accusantium architecto, molestias voluptates sequi repellendus alias ipsam nulla fuga deleniti maxime vero at labore modi natus velit maiores",
+        btnLink: "",
+    },
+    {
+        name: "Elevators",
+        image: "/images/sectors/elevators.jpg",
+        description:
+            "Lorem ipsum dolor si amet, consectetur adipisicing elit. Rerum, quam deserunt accusantium architecto, molestias voluptates sequi repellendus alias ipsam nulla fuga deleniti maxime vero at labore modi natus velit maiores",
+        btnLink: "",
+    },
+    {
+        name: "Off shore",
+        image: "/images/sectors/offshore.jpg",
+        description:
+            "Lorem ipsum dolor si amet, consectetur adipisicing elit. Rerum, quam deserunt accusantium architecto, molestias voluptates sequi repellendus alias ipsam nulla fuga deleniti maxime vero at labore modi natus velit maiores",
+        btnLink: "",
+    },
+    // {
+    //     name: "Borewell Drilling",
+    //     image: "",
+    //     description: "",
+    //     btnLink: "",
+    // },
+];
 
 export default function ProductsRangePage() {
     useEffect(() => {
@@ -35,34 +82,30 @@ export default function ProductsRangePage() {
                 titleClassName="mt-5 uppercase font-bold"
             />
             <div className={styles.cInnerContent}>
-                <RevealCard
-                    imgLink="/images/sectors/mining.jpg"
-                    title="Mining"
-                    textLeft={false}
-                    description={
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Rerum, quam deserunt accusantium architecto,
-                            molestias voluptates sequi repellendus alias ipsam
-                            nulla fuga deleniti maxime vero at labore modi natus
-                            velit maiores.
-                        </p>
-                    }
-                />
-                <RevealCard
-                    imgLink="/images/sectors/mining.jpg"
-                    title="Mining"
-                    textLeft={true}
-                    description={
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Rerum, quam deserunt accusantium architecto,
-                            molestias voluptates sequi repellendus alias ipsam
-                            nulla fuga deleniti maxime vero at labore modi natus
-                            velit maiores.
-                        </p>
-                    }
-                />
+                {sectors.map((item, ind) => {
+                    return (
+                        <RevealCard
+                            imgLink={item.image}
+                            title={item.name}
+                            textLeft={ind % 2 == 0 ? false : true}
+                            description={
+                                <>
+                                    <p className={"mb-5"}>{item.description}</p>
+                                    <Link href={item.btnLink}>
+                                        <a
+                                            className={mx(
+                                                globalStyles.btnLink,
+                                                " px-5"
+                                            )}
+                                        >
+                                            View Products
+                                        </a>
+                                    </Link>
+                                </>
+                            }
+                        />
+                    );
+                })}
             </div>
         </React.Fragment>
     );
