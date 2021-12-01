36ff84bf6ef1083b2c1b991ba213b7821dad6057
Sarwang Jain <sarwangjain05@gmail.com>
2021-12-01 10:08:47 +0530

    Designed SubSectors page

--- pages/products-range/[...slug]/index.js
+++ pages/products-range/[...slug]/index.js
@@ -4,32 +4,37 @@ import { Pages } from "@enums";
 import React from "react";
 import styles from "./index.module.css";
 import Image from "next/image";
+import Link from "next/link";
 
 import { mx } from "@utils";
 import globalStyles from "@styles/global.module.css";
 
 const branch = {
-    name: "Marble Mining",
-    caption: "Our Speciality",
+    name: "Underground Mining",
+    caption: "Premium Quality",
     description:
         "Lorem ipsum sit dolor contra voila bolario sono matro kokaneionan armei voila cora bolatro mainia filario coastania",
     products: [],
 };
 
-function Product({ imgSrc }) {
+function Product({ imgSrc, title, description }) {
     return (
         <div className={styles.productContainer}>
             <Image
                 src={imgSrc}
-                width={330}
-                height={280}
+                width={350}
+                height={300}
                 layout="fixed"
                 className={styles.productImage}
             />
             <div className={styles.productDescription}>
-                <h5 className={styles.productTitle}>
-                    Mega Beast Dyform Long 358
-                </h5>
+                <h5 className={styles.productTitle}>{title}</h5>
+                <p className="text-md">{description}</p>
+                <Link href={"/"}>
+                    <a className={mx(globalStyles.btnLink, styles.productLink)}>
+                        View Products
+                    </a>
+                </Link>
             </div>
         </div>
     );
@@ -40,6 +45,40 @@ function Product({ imgSrc }) {
 // SectionDescriptionLine -> Text that will be dangerously set
 // ProductsRangeHeading -> title
 // Products -> list of products
+
+/*
+    {
+        name: "Excavator Components",
+        classes: [
+            {
+                name: 'Bucket Teeth',
+                img: "boll.jpg",
+                description: "excavator_tooth.jpg",
+                link: ""
+            },
+            {
+                name: 'Adapters',
+                img: "boll.jpg",
+                description: "",
+                link: ""
+            },
+            {
+                name: 'Side Cutters',
+                img: "boll.jpg",
+                description: "",
+                link: ""
+            },
+            {
+                name: "Scarifier",
+                img: "scarifier.jpg",
+                description: "",
+                link: ""
+            }
+
+        ]
+    }
+
+*/
 
 export default function ProductsListPage({ market }) {
     return (
@@ -56,16 +95,85 @@ export default function ProductsListPage({ market }) {
                     {branch.description}
                 </h3>
                 <div className={styles.sectionContent}>
+                    <hr className={styles.productsSectionDivider} />
+                    {/* Buckets Section */}
                     <div className={styles.productsSection}>
                         <h4 className={styles.productsSectionTitle}>
-                            Berco Undercarriage Parts
+                            Excavator Bucket Components
                         </h4>
                         <div className={styles.productsListContainer}>
-                            {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((item) => (
-                                <Product
-                                    imgSrc={`/images/products/undercarriage/${item}.jpg`}
-                                />
-                            ))}
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_tooth.jpg"
+                                }
+                                title={"Tooth"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_adapter.jpg"
+                                }
+                                title={"Adapter"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_sidecutter.jpg"
+                                }
+                                title={"Side Cutter"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_scarifier.jpg"
+                                }
+                                title={"Scarifier"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                        </div>
+                    </div>
+                    <hr className={styles.productsSectionDivider} />
+                    {/* Undercarriage Section */}
+                    <div className={styles.productsSection}>
+                        <h4 className={styles.productsSectionTitle}>
+                            Excavator Undercarriage Components
+                        </h4>
+                        <div className={styles.productsListContainer}>
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_rollers.jpg"
+                                }
+                                title={"Rollers"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_trackshoes.jpg"
+                                }
+                                title={"Track Shoes"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_idlers.jpg"
+                                }
+                                title={"Idlers"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_trackchains.jpg"
+                                }
+                                title={"Track Chains"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
+                            <Product
+                                imgSrc={
+                                    "/images/products/classes/excavator_drivesprockets.jpg"
+                                }
+                                title={"Drive Sprockets"}
+                                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit"
+                            />
                         </div>
                     </div>
                 </div>
